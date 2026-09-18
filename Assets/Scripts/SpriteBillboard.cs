1a204b081c8ea3cd79cb3b07b7c53a4e1b5b89f8
r1k1 <yu.izanagi.persona4@pm.me>
2026-09-18 15:25:18 +1200

    asdasd

---
+++ Assets/Scripts/SpriteBillboard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SpriteBillboard : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    }
+}
